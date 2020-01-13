7e186e600e458b64f674fa1e6966d6a99ca8fbea
Levi Lovelock <lovelock.levi@gmail.com>
2020-01-12 16:13:53 -0800

    Split the HUD component to have dumb instruction comp

--- src/components/desktop/HUD.tsx
+++ src/components/desktop/HUD.tsx
@@ -1,6 +1,5 @@
-import React, {useEffect, useState} from "react";
-import useStore from '../../utils/Store';
-import DesktopController from "../../controllers/DesktopController";
+import React, {useEffect, useState} from 'react';
+import DesktopController from '../../controllers/DesktopController';
 
 interface HUDProps {
     controls: DesktopController
@@ -15,10 +14,10 @@ export const HUD = (props: HUDProps) => {
             setPointerLockedState(false);
             controls.lock(false);
         };
-        controls.plc.addEventListener("unlock", onUnlock);
+        controls.plc.addEventListener('unlock', onUnlock);
 
         return function cleanup() {
-            controls.plc.removeEventListener("unlock", onUnlock);
+            controls.plc.removeEventListener('unlock', onUnlock);
         }
     }, [controls]);
 
@@ -27,17 +26,19 @@ export const HUD = (props: HUDProps) => {
         controls.lock(true)
     };
 
-    return pointerLocked ? null :
-        <div id="blocker" onClick={setPointerLocked} style={{display: pointerLocked ? "none" : "block"}}>
-            <div id="instructions" style={{display: pointerLocked ? "none" : ""}}>
-                <span style={{fontSize: 36 + 'px'}}>Click to play</span>
-                <br/><br/>
-                Move: WASD<br/>
-                Rise: SPACE<br/>
-                Fall: CTRL<br/>
-                Look: MOUSE
-            </div>
-        </div>
+    return pointerLocked ? null : <Instructions pointerLock={setPointerLocked}/>
 };
+
+const Instructions = ({pointerLock}) =>
+    <div id='blocker' onClick={pointerLock}>
+        <div id='instructions'>
+            <span style={{fontSize: 36 + 'px'}}>Click to play</span>
+            <br/><br/>
+            Move: WASD<br/>
+            Rise: SPACE<br/>
+            Fall: CTRL<br/>
+            Look: MOUSE
+        </div>
+    </div>;
 
 export default HUD;
